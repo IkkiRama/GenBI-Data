1fe345d2373e5a7aa7168b9211ee0069b706d2ad
IkkiRama <rifkivamaus@gmail.com>
2025-01-28 10:38:46 +0700

    Adjusment

--- routes/web.php
+++ routes/web.php
@@ -5,18 +5,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-
-// use Intervention\Image\Facades\Image;
-// use Intervention\Image\Laravel\Facades\Image;
-
-Route::get('/test-intervention', function () {
-    $image = Image::canvas(800, 600, '#ff0000'); // Gambar merah ukuran 800x600
-    $image->text('Hello, World!', 400, 300, function ($font) {
-        $font->size(50);
-        $font->color('#ffffff');
-        $font->align('center');
-        $font->valign('middle');
-    });
-    return $image->response('webp'); // Tampilkan dalam format WEBP
-});
